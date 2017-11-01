9c75ab66eecd0786b3b75190266910bb1d8d32a6
Gio85 <giovanni.galiero85@gmail.com>
2017-11-01 15:19:33 +0000

    something has changed

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,4 @@
- const router = require('express').Router();
+const router = require('express').Router();
 const trips = require('../controllers/trips');
 const users = require('../controllers/users');
 const auth = require('../controllers/auth');
