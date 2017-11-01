56ca16f2f3262b4645cecd6d60425a48028525bd
Gio85 <giovanni.galiero85@gmail.com>
2017-11-01 15:59:10 +0000

    working on the navbar

--- src/components/utility/NavBar.js
+++ src/components/utility/NavBar.js
@@ -13,28 +13,31 @@ const Navbar = ({ history }) => {
 
   return(
     <nav>
-      <Link className="navLogo" to="/">Trips</Link>
       <ul className="navWrap">
-        <li className="navItem">
-          <Link to="/trips">
-            All Trips
-          </Link>
-        </li>
-        {' '}
-        <li className="navItem">
-          {Auth.isAuthenticated() && <Link to="/trips/new">
-            <i className="fa fa-plus" aria-hidden="true"></i>Add Trip
-          </Link>}
-        </li>
-        <li className="navItem">
-          {!Auth.isAuthenticated() && <Link to="/login">Login</Link>}
-        </li>
-        <li className="navItem">
-          {!Auth.isAuthenticated() && <Link to="/register">Register</Link>}
-        </li>
-        <li className="navItem">
-          {Auth.isAuthenticated() && <button onClick={logout} className="navLink">Logout</button>}
-        </li>
+        <div className="navLeft">
+          <Link className="navItem navLogo" to="/">Home</Link>
+          <li className="navItem">
+            <Link to="/trips">
+            Trips
+            </Link>
+          </li>
+        </div>
+        <div className="navRight">
+          <li className="navItem">
+            {Auth.isAuthenticated() && <Link to="/trips/new">
+              <i className="fa fa-plus" aria-hidden="true"></i>Add Trip
+            </Link>}
+          </li>
+          <li className="navItem">
+            {!Auth.isAuthenticated() && <Link to="/login">Login</Link>}
+          </li>
+          <li className="navItem">
+            {!Auth.isAuthenticated() && <Link to="/register">Register</Link>}
+          </li>
+          <li className="navItem">
+            {Auth.isAuthenticated() && <button onClick={logout} className="navLink">Logout</button>}
+          </li>
+        </div>
       </ul>
     </nav>
   );
