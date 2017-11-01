394e1c53670c3e4f9fc4cbbf6b1b88af63136063
Gio85 <giovanni.galiero85@gmail.com>
2017-11-01 16:37:08 +0000

    adding userProfile link on the navBar

--- src/components/utility/NavBar.js
+++ src/components/utility/NavBar.js
@@ -29,6 +29,9 @@ const Navbar = ({ history }) => {
             </Link>}
           </li>
           <li className="navItem">
+            {Auth.isAuthenticated() && <Link to={`/users/${Auth.getPayload().userId}`}>Profile</Link>}
+          </li>
+          <li className="navItem">
             {!Auth.isAuthenticated() && <Link to="/login">Login</Link>}
           </li>
           <li className="navItem">
