6d0ef7f02c0c4ab75586a9f081da06d8e55c9185
Gio85 <giovanni.galiero85@gmail.com>
2017-11-02 13:49:54 +0000

    styling the userPage

--- src/components/users/UsersShow.js
+++ src/components/users/UsersShow.js
@@ -28,35 +28,34 @@ class UsersShow extends React.Component {
 
   render() {
     return(
-      <div className="row box">
-        <div className="col-md-12 box-flex">
-          <div className="col-md-3">
-            <h1>{this.state.user.username}</h1>
-            <img src={this.state.user.imageSRC} />
-            {Auth.isAuthenticated() && this.props.match.params.userId !== Auth.getPayload().userId &&
-            <button
-              onClick={this.createConversation}
-              className="btn btn-outline-primary">Message
-            </button>
-            }
-          </div>
-          <div className="col-md-9">
-            <div className="box-flex">
-              <BackButton history={this.props.history} />
-              <h3>Trips</h3>
-            </div>
-            {this.state.user && this.state.user.trips.map((trip) => {
-              return (
-                <div key={trip.id} className="card">
-                  <img src={trip.imageSRC} />
-                  <Link to={`/trips/${trip.id}`}><p>{trip.name}</p></Link>
-                </div>
-              );
-            })}
-
-          </div>
+      <div className="row box box-flex">
+        <div className="col-md-3">
+          <h1>{this.state.user.username}</h1>
+          <img className="avatar" src={this.state.user.imageSRC} />
+          <p><i className="fa fa-map-marker" aria-hidden="true"></i>{this.state.user.homeLocation}</p>
+          {Auth.isAuthenticated() && this.props.match.params.userId !== Auth.getPayload().userId &&
+          <button
+            onClick={this.createConversation}
+            className="btn btn-outline-primary">Message
+          </button>
+          }
         </div>
-        <div className="col-md-12">
+        <div className="col-md-9">
+          <div className="box-flex">
+            <BackButton history={this.props.history} />
+            <h3>Trips</h3>
+          </div>
+          {this.state.user && this.state.user.trips.map((trip) => {
+            return (
+              <div key={trip.id} className="card-userPage">
+                <Link to={`/trips/${trip.id}`}>
+                  <div className="profile-img" style={{backgroundImage: `url(${trip.imageSRC})`}}>
+                    <p>{trip.name}</p>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
 
         </div>
       </div>
