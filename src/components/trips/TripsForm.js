82372a0e0e1544043bab23b3ccde10b4b0d31368
Gio85 <giovanni.galiero85@gmail.com>
2017-11-02 14:59:27 +0000

    styling the tripForm

--- src/components/trips/TripsForm.js
+++ src/components/trips/TripsForm.js
@@ -13,40 +13,42 @@ function TripsForm({ handleSubmit, handleChange, trip, errors, history }) {
           <h1>Add your Trip</h1>
         </div>
         <form onSubmit={handleSubmit} className="tripForm">
-          <div className={errors.name ? 'form-group has-error' : 'form-group'}>
-            <label htmlFor="name">Location</label>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              name="name"
-              value={trip.name}
-              onChange={handleChange}
-            />
-            {errors.name && <small className="has-error">{errors.name}</small>}
-          </div>
-          <div className={errors.description ? 'form-group has-error' : 'form-group'}>
-            <label htmlFor="description">Description</label>
-            <input
-              type="text"
-              className="form-control"
-              id="description"
-              name="description"
-              value={trip.description}
-              onChange={handleChange}
-            />
-            {errors.description && <small className="has-error">{errors.description}</small>}
-          </div>
-          <div className={errors.image ? 'form-group has-error' : 'form-group'}>
-            <label htmlFor="image">Image</label>
-            <DragDrop 
-              onChange={handleChange}
-              value={trip.base64 || trip.imageSRC}
-            />
-            {errors.image && <small className="has-error">{errors.image}</small>}
-          </div>
-          <div>
-            <button className="btn btn-primary">Save</button>
+          <div className="center col-md-8">
+            <div className={errors.name ? 'has-error' : 'form-group'}>
+              <label htmlFor="name">Location</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                value={trip.name}
+                onChange={handleChange}
+              />
+              {errors.name && <small className="has-error">{errors.name}</small>}
+            </div>
+            <div className={errors.description ? 'form-group has-error' : 'form-group'}>
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                className="form-control"
+                id="description"
+                name="description"
+                value={trip.description}
+                onChange={handleChange}
+              />
+              {errors.description && <small className="has-error">{errors.description}</small>}
+            </div>
+            <div className={errors.image ? 'form-group has-error' : 'form-group'}>
+              <label htmlFor="image">Image</label>
+              <DragDrop
+                onChange={handleChange}
+                value={trip.base64 || trip.imageSRC}
+              />
+              {errors.image && <small className="has-error">{errors.image}</small>}
+            </div>
+            <div className="form-btn">
+              <button className="btn btn-primary btn-sm">Save</button>
+            </div>
           </div>
         </form>
       </div>
