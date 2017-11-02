154da7aae42ed9c932501355bc6bf1604aff1360
Gio85 <giovanni.galiero85@gmail.com>
2017-11-02 19:31:16 +0000

    Styling the tripShow page

--- src/components/pages/HomePageIndex.js
+++ src/components/pages/HomePageIndex.js
@@ -3,7 +3,12 @@ import React from 'react';
 class HomePageIndex extends React.Component {
   render() {
     return(
-      <div className="homepage"></div>
+      <div className="homepage">
+        <div className="col-md-12">
+          <p className="center-text">Twenty years from now you will be more disappointend by the things you did not do than by the onces you did do. So trhow off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails.
+        Explore. Dream. Discover and on top of that, do not forget to share your adventure with us!</p>
+        </div>
+      </div>
     );
   }
 }
