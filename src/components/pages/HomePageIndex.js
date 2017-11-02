a456d29d702e8ba2098376b2fe5db60365edc6d0
Gio85 <giovanni.galiero85@gmail.com>
2017-11-02 19:40:06 +0000

    Working on the homepage

--- src/components/pages/HomePageIndex.js
+++ src/components/pages/HomePageIndex.js
@@ -5,8 +5,8 @@ class HomePageIndex extends React.Component {
     return(
       <div className="homepage">
         <div className="col-md-12">
-          <p className="center-text">Twenty years from now you will be more disappointend by the things you did not do than by the onces you did do. So trhow off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails.
-        Explore. Dream. Discover and on top of that, do not forget to share your adventure with us!</p>
+          <p className="center-text">Twenty years from now you will be more disappointend by the things you did not do than by the onces you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails.
+            <br /><span className="bold">Explore. Dream. Discover</span>, and first of all share your adventure with us!</p>
         </div>
       </div>
     );
