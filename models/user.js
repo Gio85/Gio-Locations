40eecbfc2ce58925a2e055acd1b98a374a90eb4c
Gio85 <giovanni.galiero85@gmail.com>
2017-11-03 09:51:13 +0000

    Updating the user model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: 'Username is required' },
-  homeLocation: { type: String, required: 'Location is required' },
+  homeLocation: { type: String },
   image: { type: String , required: 'The image is required'},
   email: { type: String, required: 'Email is required', unique: 'Email address already taken' },
   password: { type: String }
