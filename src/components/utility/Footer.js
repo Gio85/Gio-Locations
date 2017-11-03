018bf322be6856c65d704808f74e804ea6b85e2d
Gio85 <giovanni.galiero85@gmail.com>
2017-11-03 08:43:14 +0000

    changed the name

--- src/components/utility/Footer.js
+++ src/components/utility/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="hero-foot">
-      <p><strong>Lost & Found Pets</strong></p>
+      <p><strong>GioLocations</strong></p>
       <p>Made with <i className="fa fa-heartbeat"></i> at <a href="https://generalassemb.ly/locations/london" target="_blank" rel="noopener noreferrer">GA</a> WDI #29</p>
       <a href="https://github.com/Gio85" rel="noopener noreferrer" target="_blank">
         <i className="fa fa-github"></i> Giovanni Galiero
